3e7fa2f1d5f385faf21b7d2a9c82142b3840b754
Waldflamme <giglevigle@gmail.com>
2025-02-17 20:35:12 +0300

    Очко

--- Очко/PracticeTask2.py
+++
@@ -1,9 +0,0 @@
-class Person():
-    def __init__(self,name,gender,age=20):
-        self.name = name
-        self.gender = gender
-        self.age = age
-person1 = Person('Ivan','male',23)
-person2 = Person('Peter','male')
-print(person1.name,person1.gender,person1.age)
-print(person2.name,person2.gender,person2.age)
